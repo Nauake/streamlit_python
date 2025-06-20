eac1a4f76d3c6bfe97dc131b2cebb983f7889f20
Nauake <104771006+Nauake@users.noreply.github.com>
2025-06-20 20:56:57 +0000

    dashbords.py

--- dashbords.py
+++ dashbords.py
@@ -1,39 +1,3 @@
-# # import pandas as pd
-# #
-# # dados = pd.read_excel("Classificacao_teste.xlsx")
-# # # print(dados.head(30))
-# # Run this app with `python app.py` and
-# # visit http://127.0.0.1:8050/ in your web browser.
-#
-#
-# from dash import Dash, html, dcc
-# import plotly.express as px
-# import pandas as pd
-#
-# app = Dash()
-#
-# # assume you have a "long-form" data frame
-# # see https://plotly.com/python/px-arguments/ for more options
-#
-#
-# data = {'Materias':["AA TAF", "PISTOLA", "AA TEC MIL I", "AA PATRULHA II"],
-#       'Medias':[9.452, 8.187, 8.357, 8.839]
-#       }
-# df = pd.DataFrame(data)
-#
-# fig = px.bar(df, x="Materias", y="Medias", barmode="group")
-# app.layout = html.Div(children=[
-#     html.H1(children='Hello Dash'),
-#     html.H2(children="Fatura tudo"),
-#
-#     dcc.Graph(
-#         id='example-graph',
-#         figure=fig
-#     )
-# ])
-#
-# if __name__ == '__main__':
-#     app.run(debug=True)
 import streamlit as st
 import pandas as pd
 import numpy as np
